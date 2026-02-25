9af56d4020c0de150c06d110f7e5b0aa1c3e1ce2
suleman12345678 <sulemanansari2004@gmail.com>
2026-02-25 12:20:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,74 @@
+function myFunction() {
+  document.getElementById("form-container").innerHTML = "Login successful.";
+}
+
+//VARIABLES IN JAVASCRIPT
+//let x=5;
+//let y=5;
+//z=x+y;
+
+//const x=5;
+//const y =5;
+//z=x+y;
+
+//var x= 5;
+//console.log(x);
+//var y= 5;
+//console.log(y);
+//z=x+y;
+//console.log(z);
+
+//console.log('hello world');
+
+
+// IF ELSE IF ELSE CODE
+// let a = 2 ;
+ //let b = 3 ;
+ //if (a = 2) {
+  //console.log('yes');
+ //} else if (a = 1) {
+  //console.log('yes');
+ //}  else {
+  //console.log('both')
+ //}
+ 
+//SWITCH CODE
+//let day;
+//let date = new Date().getDay();
+ 
+ //switch (new Date().getDay()) {
+  //case 0:
+    //day = "Sunday";
+    //break;
+  //case 1:
+    //day = "Monday";
+    //break;
+  //case 2:
+     //day = "Tuesday";
+    //break;
+  //case 3:
+    //day = "Wednesday";
+    //break;
+  //case 4:
+    //day = "Thursday";
+    //break;
+  //case 5:
+    //day = "Friday";
+    //break;
+  //case 6:
+    //day = "Saturday";
+//}
+
+//document.getElementById("switch").innerHTML = "Today is " + day;
+
+//FOR LOOP
+//const cars = ["BMW", "Volvo", "Saab", "Ford"];
+//let len = cars.length;
+
+
+//let text = "";
+//for (let i = 0; i < len; i++) {
+ //text += cars[i] + "<br>";
+//}
+
+//document.getElementById("for").innerHTML = text;
